135970b8cd9afe3c0e637ea1493f09fe10994349
Lokesh12906 <lokesh12906@gmail.com>
2026-03-17 23:53:49 +0530

    branch init

--- src/motion_analysis/optical_flow.py
+++ src/motion_analysis/optical_flow.py
@@ -1,1 +1,13 @@
-﻿
+﻿import cv2
+
+def compute_optical_flow(frame1, frame2):
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+    flow = cv2.calcOpticalFlowFarneback(
+        gray1, gray2,
+        None,
+        0.5, 3, 15, 3, 5, 1.2, 0
+    )
+
+    return flow
